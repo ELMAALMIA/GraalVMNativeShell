92d369a67c10d7fbd2f2987a4a9f41577adb107b
ELMAALMIA <ayoubelmaalmi@gmail.com>
2024-11-25 14:09:39 +0100

    feat : create first Cli application (jar/.exe)

---
+++ src/main/java/com/dev/app/MyCustomCommand.java
@@ -0,0 +1,22 @@
+package com.dev.app;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+public class MyCustomCommand {
+    @ShellMethod(key = "hello")
+    public String hello(@ShellOption(defaultValue = "me") String name, @ShellOption(defaultValue = "1") int counter){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <counter ; i++) {
+            stringBuilder.append("Hello" +name+" iteration "+i+"\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    @ShellMethod(key = "test")
+    public String test(){
+        return  "Test Shell ";
+    }
+}
